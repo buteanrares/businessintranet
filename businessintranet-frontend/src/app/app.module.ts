6cd25f912f23abab684c5b084800e15efca42d33
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2022-05-03 20:19:37 +0300

    Feature #BUSINESS-7 Added DevExtreme

--- businessintranet-frontend/src/app/app.module.ts
+++ businessintranet-frontend/src/app/app.module.ts
@@ -1,27 +1,28 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {EmployeeComponent} from './models/employee/employee.component';
-import {LoginComponent} from './login/login.component';
-import {FormsModule} from "@angular/forms";
-import {HttpClientModule} from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { DevExtremeModule } from 'devextreme-angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeeComponent } from './models/employee/employee.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        EmployeeComponent,
-        LoginComponent
-    ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule,
-        HttpClientModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    EmployeeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    DevExtremeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
