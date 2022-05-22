7880f8f48dfe5e299a11c11b55e2ace8d37e052c
Rares Butean <59267118+buteanrares@users.noreply.github.com>
2022-05-22 21:08:49 +0300

    Unstashed changes

--- businessintranet-frontend/src/app/components/employees/service/employees.service.ts
+++ businessintranet-frontend/src/app/components/employees/service/employees.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EndpointConfig } from 'src/app/configurations/endpoint-config';
 import { EmployeeBaseModel } from 'src/app/models/employee-models/employee-base-model';
 import { EmployeeModelExtended } from 'src/app/models/employee-models/employee-model-extended';
 
